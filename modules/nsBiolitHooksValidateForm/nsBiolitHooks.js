d8faff73f908102f85f07cb9748789f7f12b3646
Pierre <pierre_delaunay@natural-solutions.eu>
2015-02-06 10:50:02 +0100

    commit

---
+++ modules/nsBiolitHooksValidateForm/nsBiolitHooks.js
@@ -0,0 +1,15 @@
+// (function ($) {
+//   Drupal.behaviors.nsBiolitHooks = {
+//     attach: function (context, settings) {
+//       // Your Javascript code goes here
+
+//       function go(){
+
+//       	//ici on va essayer d'alterer
+
+//       	//alert('oh oui');
+//       }
+ 
+//     }
+//   };
+// }(jQuery));
